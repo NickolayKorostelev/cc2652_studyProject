1c5a6fb1cd040c50dd55ca73f42ea9ca52ce98d8
Nickolai Korostelev <korostelev94@gmail.com>
2021-04-13 13:37:32 +0300

    fix: add Kalman filter files. Add data types in Axis struct. Feature: add some math to calculate sensor rotation.

--- empty.cpp
+++ empty.cpp
@@ -14,7 +14,7 @@
  *    documentation and/or other materials provided with the distribution.
  *
  * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
+ *    its contributors macceleration.y be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -26,7 +26,7 @@
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
  * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * OTHERWISE) ARISING IN ANY Wacceleration.y OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -44,32 +44,71 @@
 #include "ti_drivers_config.h"
 
 #include "I2Cdev.h"
+#include "kalman.hpp"
+#include "MPU6050.h"
+#include <Cmath>
+
+/*
+ * struct for acceleration and rotation values
+ */
+struct Aiss
+{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
 
 /*
  *  ======== mainThread ========
  */
-extern "C"
+extern "C" {
+void* mainThread(void *arg0)
 {
-    void *mainThread(void *arg0)
+    /* Call driver init functions */
+    GPIO_init();
+
+    I2Cdev *i2cSocket = new I2Cdev(I2C_400kHz, TEST_I2C);
+
+    Aiss acceleration = { 0 };
+    Aiss rotation = { 0 };
+    Aiss finalPosition = { 0 };
+
+    MPU6050 *accelerometer = new MPU6050();
+
+    Kalman *filter = new Kalman();
+
+    /* Configure the LED pin */
+    GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
+
+    /* Turn on user LED */
+    GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
+
+    time_t time = 1;
+
+    while (1)
     {
-        /* Call driver init functions */
-        GPIO_init();
+        sleep(time);
+        accelerometer->getRotation(&rotation.x, &rotation.y, &rotation.z);/*read rotation from gyroscope to local struct*/
+        accelerometer->getAcceleration(&acceleration.x, &acceleration.y,
+                                       &acceleration.z);/*read acceleration from gyroscope to local struct*/
 
-
-        I2Cdev *i2cSocket = new I2Cdev(I2C_400kHz, TEST_I2C);
-
-        /* Configure the LED pin */
-        GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
-
-        /* Turn on user LED */
-        GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
-
-        time_t time = 1;
-
-        while (1)
-        {
-            sleep(time);
-            GPIO_toggle(CONFIG_GPIO_LED_0);
-        }
+        finalPosition.x = filter->getResult(
+                (180 / 3.141592)
+                        * atanf(acceleration.x
+                                / sqrt((acceleration.y * acceleration.y)
+                                        + (acceleration.z * acceleration.z))));
+        finalPosition.y = filter->getResult(
+                (180 / 3.141592)
+                        * atanf(acceleration.y
+                                / sqrt((acceleration.x * acceleration.x)
+                                        + (acceleration.z * acceleration.z))));
+        finalPosition.z = filter->getResult(
+                (180 / 3.141592)
+                        * atanf(sqrt(
+                                (acceleration.z * acceleration.z)
+                                        + (acceleration.x * acceleration.x))
+                                / acceleration.z));
+        GPIO_toggle(CONFIG_GPIO_LED_0);
     }
+}
 }
